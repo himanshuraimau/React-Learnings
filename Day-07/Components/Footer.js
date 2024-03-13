078e1314bad88fbc26990e9ab555b88c010c897c
Himanshu Rai <1si23ad016@sit.ac.in>
2024-03-13 22:17:01 +0530

    Day-07

---
+++ Day-07/Components/Footer.js
@@ -0,0 +1,21 @@
+// Footer component for footer section
+import React from "react";
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/himanshu-rai-246121278/" target="_blank" title="Himanshu Rai Linkedin Profile">
+        Himanshu Rai
+      </a>
+      <i className="fa-solid fa-copyright"></i>
+      {year}
+      <strong>
+        Food<span>Fire</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
